cd46069eaf588b7f5697c8b612f2e24edc58e221
linyoubiao <linyoubiao@qq.com>
2020-04-19 10:06:57 +0800

    Update hello.py

--- flask/lesson001/hello.py
+++ flask/lesson001/hello.py
@@ -1,4 +1,19 @@
 """
-This is the new things
+Github and Flask example
+by LinYoUBiAo
+2020-04-19
 """
-import sys
+
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("readme.html")
+
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
+
